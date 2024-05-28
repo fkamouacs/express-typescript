6836e066274bb02134c63a2127b0eb583342bafd
fkamouacs <kamouaferid@gmail.com>
2024-05-28 19:50:21 -0400

    deploy

--- dist/index.js
+++ dist/index.js
@@ -4,8 +4,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const app = (0, express_1.default)();
 const port = process.env.PORT || 8080;
+const corsOptions = {
+    origin: '*',
+    credentials: true, //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+};
+app.use((0, cors_1.default)(corsOptions));
+app.use(express_1.default.json());
 app.get('/', (_req, res) => {
     return res.send('Express Typescript on Vercel');
 });
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
 import express, { Request, Response } from 'express';
+import cors from 'cors';
 
 const app = express();
 const port = process.env.PORT || 8080;
+
+const corsOptions = {
+  origin: '*',
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+app.use(express.json());
 
 app.get('/', (_req: Request, res: Response) => {
   return res.send('Express Typescript on Vercel');
